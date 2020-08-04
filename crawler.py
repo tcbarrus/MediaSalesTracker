9726ed75bbc30a6cc89742d67f8087b87d8d9936
Trevor Barrus <tcbarrus@gmail.com>
2020-08-04 10:48:53 -0700

    Added stub methods to trace code functionality and a crawler class

---
+++ crawler.py
@@ -0,0 +1,13 @@
+class WebCrawler:
+	def __init__(self):
+		pass
+
+	# crawl()
+	# Makes a GET request to MoviesAnywhere site to obtain raw HTML data from the "deals" page. 
+	def crawl(self):
+		pass
+
+	# Uses the raw HTML data string obtained from the crawl function to parse out sale titles and prices
+	# Title and prices stored in a dictionary
+	def parseSales(self, dataString):
+		pass
--- salesTracker.py
+++ salesTracker.py
@@ -1,6 +1,3 @@
-# GET MOVIES
-# CHECK PRICE
-# UPDATE DB
 from utilities import AppleAccessObject, DatabaseAccessObject
 import time
 
@@ -14,16 +11,31 @@ class SalesTracker:
 	def updatePrices(self):
 		sales = []
 		if self.movies is not None:
+			# Crawl "deals" page on MoviesAnywhere. Parse out sales titles and keep them in a list
+			# Create a crawler object. It will take in raw HTML and parse out the title name and price into a python dictionary
 			for m in movies:
-				price = m['Price']
+				storedPrice = m['Price']
 				trackId = data['iTunesTrackID']
-				results = self.__AAO.lookupById(trackId)
-				media = results['results'][0]
-				applePrice = media['trackHdPrice'] if 'trackHdPrice' in media else media['trackPrice']
-				if price != applePrice:
-					self.__DAO.updatePrice(media['trackName'], media['trackId'], applePrice)
-				if applePrice < price:
-					m['Price'] = applePrice
-					self.sales.append(m)
+				#If MoviesAnywhere eligible, compare title to sale titles in dictionary. 
+				if m['MoviesAnywhereElligible']:
+					#Check deals list against elligible titles
+
+					#if m['Title'] in dictionary:
+						# Extract price, compare to DB
+					pass
+				else:
+				#Else Get price from iTunes
+					results = self.__AAO.lookupById(trackId)
+					media = results['results'][0]
+					applePrice = media['trackHdPrice'] if 'trackHdPrice' in media else media['trackPrice']
+					if storedPrice != applePrice:
+						self.__DAO.updatePrice(media['trackName'], media['trackId'], applePrice)
+					if applePrice < storedPrice:
+						m['Price'] = applePrice
+						self.sales.append(m)
 				#wait four seconds to avoid Apple API call limits
 				time.sleep(4)
+
+	def sendNotifications(self):
+		#Send email or text informing user of sales
+		pass
